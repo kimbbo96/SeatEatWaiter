813de80b543c09f9bc3540003e6572ace4db97bf
francesco fornasieri <fornasieri.1692740@studenti.uniroma1.it>
2019-07-04 04:17:53 +0200

    main images

--- app/src/main/java/com/example/seateatwaiter/MainActivity.java
+++ app/src/main/java/com/example/seateatwaiter/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.seateatwaiter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
+
