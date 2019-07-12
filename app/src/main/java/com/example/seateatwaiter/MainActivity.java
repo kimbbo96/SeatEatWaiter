d9967f0275a390fd39c11d55a194d7c7c331fe2e
francesco fornasieri <fornasieri.1692740@studenti.uniroma1.it>
2019-07-12 13:15:13 +0200

    login

--- app/src/main/java/com/example/seateatwaiter/MainActivity.java
+++ app/src/main/java/com/example/seateatwaiter/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.seateatwaiter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static int TIME_OUT = 1500;
@@ -14,15 +16,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences preferences;
+        preferences = getSharedPreferences("loginref", MODE_PRIVATE);
+        boolean savelogin = preferences.getBoolean("savelogin", false);
+        if (!savelogin) { // se non è loggato
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, Home.class);
-                startActivity(i);
-                finish();
-            }
-        }, TIME_OUT);
+            Toast.makeText(this, "prima volta", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(MainActivity.this, Login.class);
+            startActivity(i);
+            finish();
+
+        }
+        else {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(MainActivity.this, Home.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, TIME_OUT);
+        }
     }
 }
 
